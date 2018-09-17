fd7ebc10f43ff3f636d31c04d11daac70f7848cd
insanemax <tm_4@mail.ru>
2018-09-17 15:34:11 +0300

    сделал вывод пинов и карточки

--- js/map.js
+++ js/map.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var titles = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
@@ -16,6 +18,13 @@ var types = [
   'bungalo'
 ];
 
+var offerType = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало'
+};
+
 var times = [
   '12:00',
   '13:00',
@@ -32,9 +41,9 @@ var features = [
 ];
 
 var photos = [
-  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
 var coordinates = {
@@ -45,6 +54,20 @@ var coordinates = {
 var guests = {
   min: 1,
   max: 10
+};
+
+var numberPins = 8;
+var pinY = 65;
+var offer = [];
+var offerTitles = titles.slice();
+
+// Вычисление смещения пина из-за его размеров
+function pinOffsetX(x) {
+  return x + 'px';
+}
+
+function pinOffsetY(y) {
+  return (y - pinY) + 'px';
 }
 
 var map = document.querySelector('.map');
@@ -55,25 +78,26 @@ var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map_
 var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var fragment = document.createDocumentFragment();
 
-
-//случайное число
+// случайное число
 
 function getRandomNumber(min, max) {
   return min + Math.floor(Math.random() * (max - min));
 }
 
-//случайный элемент массива
+// случайный элемент массива
 
 function gerRandomElemFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//перемешивание массива
+// перемешивание массива
 
-function shuffle(arr){
-  var j, temp;
-  for(var i = arr.length - 1; i > 0; i--){
-    j = Math.floor(Math.random()*(i + 1));
+function shuffle(arr) {
+  var j;
+  var temp;
+
+  for (var i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
     temp = arr[j];
     arr[j] = arr[i];
     arr[i] = temp;
@@ -81,20 +105,24 @@ function shuffle(arr){
   return arr;
 }
 
-//массив случайной длины
+// массив случайной длины
+function makeFeatures() {
+  var arrAdvantages = features.slice();
+  var lengthArrRandom = getRandomNumber(Math.round(arrAdvantages.length / 2), arrAdvantages.length);
+  var offerFeatures = [];
 
- var makeFeatures = function () {
-    var arrAdvantages = features.slice();
-    var lengthArrRandom = getRandomNumber(Math.round(arrAdvantages.length / 2), arrAdvantages.length);
-    var offerFeatures = [];
+  for (var i = 0; i <= lengthArrRandom; i++) {
+    var indexRandom = getRandomNumber(0, arrAdvantages.length);
+    offerFeatures[i] = arrAdvantages.splice(indexRandom, 1);
+  }
 
-    for (var i = 0; i <= lengthArrRandom; i++) {
-      var indexRandom = getRandomNumber(0, arrAdvantages.length);
-      offerFeatures[i] = arrAdvantages.splice(indexRandom, 1);
-    }
+  return offerFeatures;
+}
 
-    return offerFeatures;
-  };
+// Создадим строку для вставки преимущества
+function getStringFeatures(elem) {
+  return '<li class="popup__feature popup__feature--' + elem + '"></li>';
+}
 
 function generateAds(numberOfItems) {
   var posts = [];
@@ -105,10 +133,10 @@ function generateAds(numberOfItems) {
 
     posts.push({
       author: {
-        avatar: 'img/avatar/user0' + avatarPrefix + '.png',
+        avatar: 'img/avatars/user0' + avatarPrefix + '.png',
       },
       offer: {
-        title: titles[i],
+        title: offerTitles.splice(getRandomNumber(0, offerTitles.length), 1),
         address: locationX + ', ' + locationY,
         price: getRandomNumber(1000, 1000000),
         type: gerRandomElemFromArray(types),
@@ -118,25 +146,25 @@ function generateAds(numberOfItems) {
         checkout: gerRandomElemFromArray(times),
         features: makeFeatures(),
         description: '',
-        photos: shuffle(photos) //все обьекты с одним и тем же порядком фотографий
+        photos: shuffle(photos) // все обьекты с одним и тем же порядком фотографий
       },
       location: {
         x: locationX,
         y: locationY
       }
-    })
+    });
   }
   return posts;
 }
 
 // формирование метки для объекта
 
-function generatePin(pin) {
-  mapPinElement = mapPinTemplate.cloneNode(true);
+function renderPin(pin) {
+  var mapPinElement = mapPinTemplate.cloneNode(true);
   mapPinElement.querySelector('img').src = pin.author.avatar;
-  mapPinElement.querySelector('img').alt = pin.author.title;
-  mapPinElement.style.left = pin.location.x +'px';
-  mapPinElement.style.top = pin.location.y + 'px';
+  mapPinElement.querySelector('img').alt = pin.offer.title;
+  mapPinElement.style.left = pinOffsetX(pin.location.x);
+  mapPinElement.style.top = pinOffsetY(pin.location.y);
   return mapPinElement;
 }
 
@@ -146,7 +174,7 @@ function renderCard(offerObject) {
   var mapElement = mapCardTemplate.cloneNode(true);
   var mapCardP = mapElement.querySelectorAll('p');
   var mapCardList = mapElement.querySelector('.popup__features');
-    // заголовок объявления
+  // заголовок объявления
   mapElement.querySelector('.popup__title').textContent = offerObject.offer.title;
   // цена
   mapElement.querySelector('.popup__text--price').innerHTML = offerObject.offer.price + '&#x20bd;/ночь';
@@ -154,6 +182,9 @@ function renderCard(offerObject) {
   mapElement.querySelector('.popup__text--address').textContent = offerObject.offer.address;
   // тип
   mapElement.querySelector('.popup__type').textContent = offerType[offerObject.offer.type];
+
+  mapElement.querySelector('.popup__photos img').src = offerObject.offer.photos[2];
+
   // количество гостей
   mapCardP[2].textContent = offerObject.offer.rooms + ' комнаты для ' + offerObject.offer.guests + ' гостей';
   // время заезда и выезда
@@ -165,4 +196,20 @@ function renderCard(offerObject) {
   mapElement.appendChild(mapCardList);
 
   return mapElement;
-  };
+}
+
+// Создаем и заполняем данными массив объектов недвижимости
+offer = generateAds(numberPins);
+// Переносим данные из массива объектов во фрагмент с маркерами для вставки на страницу
+offer.forEach(function (elem) {
+  fragment.appendChild(renderPin(elem));
+});
+
+// Добавляем маркеры на страницу
+pinsContainer.appendChild(fragment);
+// Создаем новый пустой фрагмент
+fragment = document.createDocumentFragment();
+// Заполняем фрагмент данными из массива объектов для отрисовки первой карточки недвижимости
+fragment.appendChild(renderCard(offer[0]));
+// Добавляем карточку недвижимости на страницу
+map.appendChild(fragment);
